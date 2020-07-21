734bafc57be535d2a2609e69b53cbc10322e75f4
Ankit Chaudhary <66492079+ankit114211@users.noreply.github.com>
2020-07-21 21:37:33 +0530

    Handle Click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
+  const [haedingText, setHeading] = useState("");
 
   function handleChange(event) {
     console.log(event.target.value);
     setName(event.target.value);
   }
 
+  function handleClick() {
+    setHeading(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
+      <h1>Hello {haedingText}</h1>
       <input
         onChange={handleChange}
         type="text"
         placeholder="What's your name?"
+        value={name}
       />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
